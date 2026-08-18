f00bccc58ed843e5092a75dabcdf034471e00584
JOÃO VITTOR NEVES DE SALES <26171615@edu119.local>
2026-08-18 11:40:43 -0300

    salvando alterações

--- 01-funcoes/06-function-expression.js
+++ 01-funcoes/06-function-expression.js
@@ -6,5 +6,9 @@ const somarAnonima = function (a, b){
     return a + b;
 }
 
+const arrowFunction = (a,b) =>{
+    return a + b
+}
 console.log(somarTradicional(10,5));
 console.log(somarAnonima(10,5));
+console.log(arrowFunction(10, 5))
