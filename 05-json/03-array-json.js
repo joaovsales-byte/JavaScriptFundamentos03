30e4d392a875c3e67591f1379180263bf2210977
JOÃO VITTOR NEVES DE SALES <26171615@edu119.local>
2026-08-25 08:57:57 -0300

    salvando alterações

--- 05-json/03-array-json.js
+++ 05-json/03-array-json.js
@@ -2,3 +2,11 @@ const alunos = [
     {nome: "Ana", nota: 9},
     {nome: "Alice", nota: 7},
 ];
+
+alunos.forEach((a)=> console.log(`A aluna ${a.nome} a nota: ${a.nota}`));
+
+for(let i = 0; i < alunos.length; i++){
+    console.log(`A aluna ${alunos[i].nome} a nota: ${alunos[i].nota}`);
+}
+const json = JSON.stringify(alunos);
+console.log(json);
