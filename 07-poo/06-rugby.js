a0b98bad11489b2fa801dd986c1a1d736529b2fa
JOÃO VITTOR NEVES DE SALES <26171615@edu119.local>
2026-08-25 10:59:13 -0300

    salvando alterações

---
+++ 07-poo/06-rugby.js
@@ -0,0 +1,27 @@
+class JogadorRugby{
+    constructor(nome, numero, posicao){
+        this.nome = nome;
+        this.numero = numero;
+        this.posicao = posicao;
+    }
+
+    correr(){
+        console.log(`${this.nome} está correndo com a bola.`);
+    }
+
+    passarBola(){
+        console.log(`${this.nome} passou a bola`);
+    }
+
+    mostrarDados(){
+        console.log(`Nome: ${this.nome}`);
+        console.log(`Número: ${this.numero}`);
+        console.log(`Posição: ${this.posicao}`);
+    }
+}
+
+const jogador1 = new JogadorRugby("Pablo",10,"Abertura");
+
+jogador1.mostrarDados();
+jogador1.correr();
+jogador1.passarBola()
